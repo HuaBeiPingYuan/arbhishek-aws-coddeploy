f4a2951261c505c2cb4e6ff1921b781f4ab3b743
HuaBeiPingYuan <yaohua13liluye@gmail.com>
2025-11-21 12:26:24 +0200

    change the stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-docker pull shichao31/simple-python-flask-app
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 shichao31/simple-python-flask-app
+echo "Hi"
